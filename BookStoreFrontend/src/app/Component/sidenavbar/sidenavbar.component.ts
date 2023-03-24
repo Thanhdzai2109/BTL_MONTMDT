246f92373ddfabca5e9e4ababbc7bb0b1f144389
thanhnt <thanhnt@gmail.com>
2023-03-25 00:18:13 +0700

    thanhntcommit

--- BookStoreFrontend/src/app/Component/sidenavbar/sidenavbar.component.ts
+++ BookStoreFrontend/src/app/Component/sidenavbar/sidenavbar.component.ts
@@ -4,6 +4,8 @@ import { Router, ActivatedRoute,ParamMap} from '@angular/router';
 
 import { Component, OnInit ,ViewChild  } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { WishlistService } from 'src/app/Service/wishlist.service';
+import { CartService } from 'src/app/Service/cart.service';
 
 
 
@@ -16,15 +18,49 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 export class SidenavbarComponent implements OnInit {
   @ViewChild('sidenav', { static: true }) public sidenav: MatSidenavModule;
   isSeller = false;
+  isUser = false;
   isAdmin=false;
   role:string;
-  
-  constructor(private router:Router,private route:ActivatedRoute,) { }
+  isLogin = false;
+  name: any;
+  length: any;
+  wishlistLength: number;
+  constructor(private router:Router,private route:ActivatedRoute, private wishlistService: WishlistService, private cartService: CartService,) { }
 
 
 
 
   ngOnInit() {
+    this.wishlistService.autoRefresh$.subscribe(() => {
+
+      this. getWishlistCount();
+    }
+
+    );
+    this. getWishlistCount();
+
+    this.cartService.autoRefresh$.subscribe(() => {
+      this.getCartItemCount();
+
+    });
+
+    this.getCartItemCount();
+    this.name = localStorage.getItem('Name');
+    this.role = localStorage.getItem('role');
+    console.log('role check toolbar', this.role);
+    if (this.role === 'admin') {
+     this.isAdmin = true;
+     this.isLogin = true;
+   }
+    if (this.role === 'seller') {
+     this.isSeller = true;
+     this.isLogin = true;
+   }
+    if (this.role === 'user') {
+     this.isUser = true;
+     this.isLogin = true;
+     console.log('is user ', this.isUser);
+   }
    this.role= localStorage.getItem('role');
    console.log('role check sidenav',this.role);
    if (this.role === 'admin') 
@@ -70,6 +106,18 @@ export class SidenavbarComponent implements OnInit {
   orderStatus(){
 
 this.router.navigate(['books'],{queryParams:{book:'order'}});
+  }
+  getWishlistCount() {
+    this.wishlistService.getWishlistCount().subscribe((response: any) => {
+      this.wishlistLength = response.obj;
+      console.log('total number wishBook are' + response.obj);
+     });
+  }
+  getCartItemCount() {
+    this.cartService.getCartItemCount().subscribe((response: any) => {
+      this.length = response.obj;
+      console.log('total number of itemes are' + response.obj);
+     });
   }
   
 }
