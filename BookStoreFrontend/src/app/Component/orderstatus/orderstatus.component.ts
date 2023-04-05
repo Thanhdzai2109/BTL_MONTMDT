49d8539c41db3b1c3d528008f4c8ab4d934813b6
thanhnt <thanhnt@gmail.com>
2023-04-05 16:02:56 +0700

    thanhntcommit

--- BookStoreFrontend/src/app/Component/orderstatus/orderstatus.component.ts
+++ BookStoreFrontend/src/app/Component/orderstatus/orderstatus.component.ts
@@ -8,6 +8,7 @@ import {  MatDialog } from '@angular/material/dialog';
 import { BookModule } from 'src/app/Model/book/book.module';
 import { Order} from 'src/app/Model/order.model';
 import { AdminService } from 'src/app/Service/admin.service';
+import { Excel, ExcelService } from 'src/app/Service/excel.service';
 
 interface Food {
   value: string;
@@ -23,6 +24,7 @@ interface Food {
 export class OrderstatusComponent implements OnInit {
 
   constructor(private service: BookService ,private adminservice:AdminService ,private dialog: MatDialog,
+    private _exporHelperService: ExcelService,
     private matSnackBar: MatSnackBar,private sellerService:BookService
 
 ) { }
@@ -128,7 +130,112 @@ updateOrderSeller(orderId:any,status:any) {
     }
   );
 }
+doExport(){
 
+  let headerTTThietHai: any[] = [
+    '',
+    '',
+    '500KV',
+    '220kv',
+    '110KV',
+    '35kv',
+    '22kv',
+    '10kv',
+    '6kv',
+    'Tổng',
+];
+let groupHeaderTTThietHai: any[] = [];
+let groupHeaderTTThietHai_row1: any[] = [
+    'TT',
+    'Tên đơn vị',
+    'Phân loại theo cấp điện áp',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    'Tổng',
+];
+groupHeaderTTThietHai.push(groupHeaderTTThietHai_row1);
+let groupMergerTTThietHai: any[] = ['A3:A4', 'B3:B4', 'C3:I3', 'J3:J4'];
+let keyTTThietHai: any[] = [
+    'TT',
+    'DV',
+    '500',
+    '220',
+    '110',
+    '35',
+    '22',
+    '10',
+    '6',
+    'TONG',
+];
+let keyTotal: any[] = [
+    '',
+    '',
+    '500',
+    '220',
+    '110',
+    '35',
+    '22',
+    '10',
+    '6',
+    'TONG',
+];
+let nameKeyTotal ="DV";
+let nameTotal="EVN"
+let dataTemp: any[] = [];
+this.orderedBooks.forEach((element, index) => {
+//   const sum=(this.analyticsDataDetail(element,"500kv") == null ?0:this.analyticsDataDetail(element,"500kv"))+
+//   (this.analyticsDataDetail(element,"220kv") == null ?0:this.analyticsDataDetail(element,"220kv"))+
+//   (this.analyticsDataDetail(element,"110kv") == null ?0:this.analyticsDataDetail(element,"110kv"))+
+//  (this.analyticsDataDetail(element,"35kv") == null ?0:this.analyticsDataDetail(element,"35kv"))+
+//  ( this.analyticsDataDetail(element,"22kv") == null ?0:this.analyticsDataDetail(element,"22kv"))+
+//  ( this.analyticsDataDetail(element,"10kv") == null ?0:this.analyticsDataDetail(element,"10kv"))+
+//  ( this.analyticsDataDetail(element,"6kv") == null ?0:this.analyticsDataDetail(element,"6kv"))
+
+  let item={
+    // "TT":index+1,
+    // "DV":element.orgName,
+    // "500":this.analyticsDataDetail(element,"500kv") == null ?0:this.analyticsDataDetail(element,"500kv"),
+    // "220":this.analyticsDataDetail(element,"220kv") == null ?0:this.analyticsDataDetail(element,"220kv"),
+    // "110":this.analyticsDataDetail(element,"110kv")==null?0:this.analyticsDataDetail(element,"110kv"),
+    // "35":this.analyticsDataDetail(element,"35kv")==null?0:this.analyticsDataDetail(element,"35kv"),
+    // "22":this.analyticsDataDetail(element,"22kv")==null?0:this.analyticsDataDetail(element,"22kv"),
+    // "10":this.analyticsDataDetail(element,"10kv")==null?0:this.analyticsDataDetail(element,"10kv"),
+    // "6":this.analyticsDataDetail(element,"6kv")==null?0:this.analyticsDataDetail(element,"6kv"),
+    // "TONG":sum
+  }
+  dataTemp.push(item)
+})
+
+
+let widthThietHai: any[] = [8, 30, 10, 10, 10, 10, 10, 10, 10, 20];
+let excelTTThietHai: Excel = {
+    title: 'Phân loại vi phạm trong HLALĐCA',
+    subTitle: null,
+    workSheet: null,
+    keys: keyTTThietHai,
+    widths: widthThietHai,
+    data: dataTemp,
+    groupHeaders: groupHeaderTTThietHai,
+    groupMerge: groupMergerTTThietHai,
+    sheetName: 'Phân loại vi phạm trong HLALĐCA',
+    headers: headerTTThietHai,
+};
+let arrayExcel = [];
+arrayExcel.push(excelTTThietHai);
+
+let timeSpan = new Date().toISOString();
+this._exporHelperService.generateExcelWithTotalRow(
+    'Vi-pham-hlat' + timeSpan,
+    arrayExcel,
+    keyTotal,
+    nameKeyTotal,
+    nameTotal
+);
+}
 
 // getInProgressOrderedBooks() {
 //   console.log('Get in progress order books -------------');
